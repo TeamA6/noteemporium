268c6db916da7459c200e0201c5e20aee53e7b3f
unknown <2083089b@BO715-2-03.ad.dcs.gla.ac.uk>
2015-03-17 16:06:59 +0000

    Database doesn't work

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from note.models import UserProfile, Note, Rating
 
-# Register your models here.
+# class NoteAdmin(admin.ModelAdmin):
+    # list_display = ('title','note_id','subject', 'module', 'date', 'format')
+
+# class RatingAdmin(admin.ModelAdmin):
+    # list_display = ('note','stars')
+
+# class UserProfileAdmin(admin.ModelAdmin):
+    # list_display = ('email','name','surname')
+
+
+# admin.site.register(Rating, RatingAdmin)
+# admin.site.register(Note, NoteAdmin)
+# admin.site.register(UserProfile)
+admin.site.register(Note)
+admin.site.register(Rating)
