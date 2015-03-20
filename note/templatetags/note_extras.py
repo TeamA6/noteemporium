be94d6d9f60d7cb49cffa36b0473e0ed4b30a7ce
unknown <2083089b@BO706-04.ad.dcs.gla.ac.uk>
2015-03-20 14:20:18 +0000

    Now working

--- note/templatetags/note_extras.py
+++ note/templatetags/note_extras.py
@@ -4,5 +4,6 @@ from note.models import Subject
 register = template.Library()
 
 @register.inclusion_tag('noteemp/subs.html')
-def get_subject_list():
-    return {'subs': Subject.objects.all()}
+
+def get_subject_list(sub=None):
+    return {'subs': Subject.objects.all(), 'act_sub': sub}
