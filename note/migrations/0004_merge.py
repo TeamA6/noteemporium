791273a3ea2dd7b2ffbde96466fdc8bf388e571e
Lorenzo <2083089b@student.gla.ac.uk>
2015-03-21 15:18:54 +0000

    Working website

---
+++ note/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('note', '0003_auto_20150319_1356'),
+        ('note', '0003_auto_20150319_1340'),
+    ]
+
+    operations = [
+    ]
