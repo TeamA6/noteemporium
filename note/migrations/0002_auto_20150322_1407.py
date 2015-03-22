cef307e868455f3bc13ce0da0eb98a2cf4558948
2079532k-laptop <bkav93@gmail.com>
2015-03-22 14:57:17 +0000

    register.html updated

---
+++ note/migrations/0002_auto_20150322_1407.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('note', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='document',
+            name='docfile',
+            field=models.FileField(upload_to=b'documents/%Y/%m/%d'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='note',
+            name='date',
+            field=models.DateField(verbose_name=b'22/03/2015 14:07'),
+            preserve_default=True,
+        ),
+    ]
