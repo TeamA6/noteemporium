96c69c9063f0358e8e71158382055a4a71eb774a
TeamA6 <lorenzobetto@yahoo.co.uk>
2015-03-26 20:58:02 +0000

    Update search.py

--- note/search.py
+++ note/search.py
@@ -3,37 +3,22 @@ import re
 from django.db.models import Q
 from models import *
 
-##def normalize_query(query_string,
-##                    findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
-##                    normspace=re.compile(r'\s{2,}').sub):
-##    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-##        and grouping quoted words together.
-##        Example:
-##        
-##        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-##        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
-##    
-##    '''
-##    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
-
 def get_query(query_string):
-    ''' Returns a query, that is a combination of Q objects. That combination
-        aims to search keywords within a model by testing the given search fields.
-    '''
     found_notes = Note.objects.filter(title__contains=query_string)
-    found = list(found_notes)
-##    for term in terms:
-##        or_query = None # Query to search for a given term in each field
-##        for field_name in search_fields:
-##            q = Q(**{"%s__icontains" % field_name: term})
-##            if or_query is None:
-##                or_query = q
-##            else:
-##                or_query = or_query | q
-##        if query is None:
-##            query = or_query
-##        else:
-##            query = query & or_query
-    #print found, "hello"
-    #print type(found)
-    return found
+    found_notes = list(found_notes)
+    return found_notes
+
+#def get_author(query_string):
+#    found_authors = UserProfile.objects.filter(user__username__contains=query_string)
+#    found_authors = list(found_authors)
+#    return found_authors
+
+def get_subject(query_string):
+    found_subjects = Note.objects.filter(subject__contains=query_string)
+    found_subjects = list(found_subjects)
+    return found_subjects
+
+def get_module(query_string):
+    found_modules = Note.objects.filter(module__contains=query_string)
+    found_modules = list(found_modules)
+    return found_modules
